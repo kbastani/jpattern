ad5826aa4cb69342e67f79d0cedfd2de5423cffb
Kenny Bastani <kb@socialmoon.com>
2014-06-09 09:08:42 -0700

    Initial commit
    Added basic pattern recognition algorithms and tests

---
+++ jpattern/kernel/src/main/java/org/recognition/kernel/impl/util/Recognizer.java
@@ -0,0 +1,109 @@
+package org.recognition.kernel.impl.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+/**
+ * This class is a utility class that performs low-level pattern recognition in a sequence of characters
+ */
+public class Recognizer {
+
+    /**
+     * This method is a helper method to print all the patterns of a symbol sequence */
+    public static void PrintRedundancyMatrix(String pattern)
+    {
+        List<HashMap<Integer, HashMap<String, Integer>>> redundancyMatrix = CountRedundancies(GetMap(Parse(pattern)));
+
+        for (int i = 0; i < redundancyMatrix.size(); i++) {
+            HashMap<Integer, HashMap<String, Integer>> redundancyList = redundancyMatrix.get(i);
+            System.out.println();
+            redundancyList.forEach(new BiConsumer<Integer, HashMap<String, Integer>>() {
+                @Override
+                public void accept(Integer integer, HashMap<String, Integer> stringIntegerHashMap) {
+                    System.out.print(stringIntegerHashMap);
+                }
+            });
+        }
+    }
+
+    // Find the features from a string and generate a redundancy matrix, scans maximum 8 symbol window
+    public static List<HashMap<Integer, HashMap<String, Integer>>> FindFeatures(String pattern)
+    {
+        return CountRedundancies(GetMap(Parse(pattern)));
+    }
+
+    public static List<String> Parse(String pattern)
+    {
+        List<String> stringList = new ArrayList<String>();
+
+        // Scan through the string
+        for (int i = 0; i < pattern.length() + 1; i++) {
+            // Create a dependency tree by scan
+            for (int j = Math.max(i - 8, 0); j < i; j++) {
+                // Check stringList for pattern
+                String val = pattern.substring(j, i);
+
+                // stringList contains val?
+                if(!stringList.contains(val))
+                    stringList.add(val);
+            }
+        }
+
+        return stringList;
+    }
+
+    public static HashMap<Integer, List<String>> GetMap(List<String> stringList) {
+        HashMap<Integer, List<String>> stringMap = new HashMap<Integer, List<String>>();
+
+        // Iterate through list
+        for (int i = 0; i < stringList.size(); i++) {
+
+            int size = stringList.get(i).length();
+
+            if (!stringMap.containsKey(size))
+                stringMap.put(size, new ArrayList<String>());
+
+            stringMap.get(size).add(stringList.get(i));
+        }
+
+        return  stringMap;
+    }
+
+    public static HashMap<Integer, HashMap<String, Integer>> CountRedundancies(List<String> stringList)
+    {
+        HashMap<Integer, HashMap<String, Integer>> countMatrix = new HashMap<Integer, HashMap<String, Integer>>();
+
+        // Count each of the redundancies
+        for (int i = 0; i < stringList.size(); i++) {
+
+            int size = stringList.get(i).length();
+
+            for (int j = 0; j < size; j++) {
+                Character symbol = stringList.get(i).toCharArray()[j];
+
+                if (!countMatrix.containsKey(j))
+                    countMatrix.put(j, new HashMap<String, Integer>());
+
+                if (!countMatrix.get(j).containsKey(symbol.toString()))
+                    countMatrix.get(j).put(symbol.toString(), 0);
+
+                int val = countMatrix.get(j).get(symbol.toString());
+
+                countMatrix.get(j).put(symbol.toString(), val + 1);
+            }
+        }
+
+        return countMatrix;
+    }
+
+    public static List<HashMap<Integer, HashMap<String, Integer>>> CountRedundancies(HashMap<Integer, List<String>> stringMap) {
+        List<HashMap<Integer, HashMap<String, Integer>>> redundancyMatrix = new ArrayList<HashMap<Integer, HashMap<String, Integer>>>();
+
+        for (int i = 1; i <= stringMap.keySet().size(); i++) {
+            redundancyMatrix.add(CountRedundancies(stringMap.get(i)));
+        }
+        return redundancyMatrix;
+    }
+}
---
+++ jpattern/kernel/src/test/java/org/recognition/kernel/impl/util/RecognizerTest.java
@@ -0,0 +1,78 @@
+package org.recognition.kernel.impl.util;
+
+import com.sun.istack.internal.NotNull;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class RecognizerTest extends TestCase {
+
+    @Test
+    public void testParse() throws Exception {
+        @NotNull
+        List<String> actual = Recognizer.Parse("101");
+        List<String> expected = Arrays.asList("1", "10", "0", "101", "01");
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testSampleParse() throws Exception {
+        @NotNull
+        List<String> actual = Recognizer.Parse("0101");
+        System.out.println(actual);
+    }
+
+    @Test
+    public void testGetMap() throws Exception {
+        List<String> input = Arrays.asList("1", "10", "0", "101", "01");
+
+        HashMap<Integer, List<String>> expected = new HashMap<Integer, List<String>>();
+
+        expected.put(1, Arrays.asList("1", "0"));
+        expected.put(2, Arrays.asList("10", "01"));
+        expected.put(3, Arrays.asList("101"));
+
+        assertEquals(expected, Recognizer.GetMap(input));
+    }
+
+    @Test
+    public void testCountRedundancies() throws Exception {
+
+        List<String> input = Arrays.asList("1", "10", "0", "101", "01");
+        HashMap<Integer, HashMap<String, Integer>> expected = new HashMap<Integer, HashMap<String, Integer>>();
+
+        expected.put(0, new HashMap<String, Integer>());
+        expected.put(1, new HashMap<String, Integer>());
+        expected.put(2, new HashMap<String, Integer>());
+        expected.get(0).put("0", 2);
+        expected.get(0).put("1", 3);
+        expected.get(1).put("0", 2);
+        expected.get(1).put("1", 1);
+        expected.get(2).put("1", 1);
+
+        assertEquals(expected, Recognizer.CountRedundancies(input));
+    }
+
+    @Test
+    public void testCountRedundancies1() throws Exception {
+        HashMap<Integer, List<String>> input = new HashMap<Integer, List<String>>();
+
+        input.put(1, Arrays.asList("1", "0"));
+        input.put(2, Arrays.asList("10", "01"));
+        input.put(3, Arrays.asList("101"));
+
+        List<HashMap<Integer, HashMap<String, Integer>>> expected = new ArrayList<HashMap<Integer, HashMap<String, Integer>>>();
+
+        for (int i = 1; i <= input.keySet().size(); i++) {
+            expected.add(Recognizer.CountRedundancies(input.get(i)));
+        }
+
+        assertEquals(expected, Recognizer.CountRedundancies(input));
+    }
+}
